efdc03348bf23530f879db0dfc3eca69766b8658
peachberry <peachberry0318@gmail.com>
2020-04-21 21:56:28 +0900

    Initial Commit

---
+++ MyChurch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyChurch
+//
+//  Created by 백승화 on 2020/04/21.
+//  Copyright © 2020 백승화. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
